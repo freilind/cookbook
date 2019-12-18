d425a0275369b388a6fa259f64044d8e83fdd5ea
Freilin Manzano <freilin.manzano@externos.bci.cl>
2019-12-18 09:57:43 -0300

    add components of project.

---
+++ src/app/shared/data-storage.service.ts
@@ -0,0 +1,46 @@
+import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { map, tap } from 'rxjs/operators';
+
+import { Recipe } from '../recipes/recipe.model';
+import { RecipeService } from '../recipes/recipe.service';
+import { environment } from '../../environments/environment'
+
+@Injectable({ providedIn: 'root' })
+export class DataStorageService {
+
+  private host : String = environment.db.host;
+  constructor(private http: HttpClient, private recipeService: RecipeService) {}
+
+  storeRecipes() {
+    const recipes = this.recipeService.getRecipes();
+    this.http
+      .put(
+        `${this.host}recipes.json`,
+        recipes
+      )
+      .subscribe(response => {
+        console.log(response);
+      });
+  }
+
+  fetchRecipes() {
+    return this.http
+      .get<Recipe[]>(
+        `${this.host}recipes.json`
+      )
+      .pipe(
+        map(recipes => {
+          return recipes.map(recipe => {
+            return {
+              ...recipe,
+              ingredients: recipe.ingredients ? recipe.ingredients : []
+            };
+          });
+        }),
+        tap(recipes => {
+          this.recipeService.setRecipes(recipes);
+        })
+      )
+  }
+}
